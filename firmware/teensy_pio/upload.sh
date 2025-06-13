34b0a20a560db268c1249d2b03adab1d3f3e54d9
Alyss- Fielding <af497@byu.edu>
2025-06-12 18:53:25 -0600

    added libusb for the tycmd for teensy communication

--- firmware/teensy_pio/upload.sh
+++ firmware/teensy_pio/upload.sh
@@ -3,21 +3,21 @@
 #
 # Builds and uploads the latest micro-ROS code to the Teensy board
 
-# cd ~/firmware/teensy_pio
-# pio run
-
-# cd ~/firmware/teensy_pio/.pio/build/teensy41
-# tycmd upload firmware.hex
-
-
 cd ~/firmware/teensy_pio
 pio run
 
-echo "Please press the RESET button on the Teensy NOW to enter bootloader mode..."
-read -p "Press Enter after pressing reset..."
-
 cd ~/firmware/teensy_pio/.pio/build/teensy41
 tycmd upload firmware.hex
+
+
+# cd ~/firmware/teensy_pio
+# pio run
+
+# echo "Please press the RESET button on the Teensy NOW to enter bootloader mode..."
+# read -p "Press Enter after pressing reset..."
+
+# cd ~/firmware/teensy_pio/.pio/build/teensy41
+# tycmd upload firmware.hex
 
 # cd ~/firmware/teensy_pio
 # pio run --target upload
